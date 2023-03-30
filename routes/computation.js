aee42027e9c00d02dde8dd3a3b568a28fa9876ef
kirni13 <soujanyakirni13@gmail.com>
2023-03-29 22:37:45 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var tang =Math.tan()*100;
+
+var l=0;
+var m=0;
+var n=0;
+var o=0;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  l=Math.acosh(tang);
+  m=Math.log1p();
+  n=Math.tan(tang);
+  o=Math.sqrt(tang);
+
+  res.send(`acosh applied to : ${tang} is ${l} <br> log1p applied to : ${tang} is ${m} <br> tan applied to : ${tang} is ${n} <br> sqrt applied to : ${tang} is ${o}`);
+ 
+});
+
+module.exports = router;
